be23faa4e66087efea115bcdb5ec6513a5d20027
ruksana27mikey-ship-it <ruksana27mikey@gmail.com>
2026-02-03 16:09:08 +0530

    Implement binary search in descending array

---
+++ binarysearchinc.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={19,18,17,16,15};
+	int num,beg=0,end=4,mid,found=0;
+	printf("enter a number to be searched: ");
+	scanf("%d",&num);
+	while(beg<=end)
+	{
+		mid=(beg+end)/2;
+		if(a[mid]==num)
+		{
+		
+			printf("number found at position %d",mid+1);
+			found=1;
+			break;
+		}
+		else if(num>a[mid])
+		{
+			end=mid-1;
+		}
+		else if(num<a[mid])
+		{
+			beg=mid+1;
+		}
+	}
+	if (found==0)
+	{
+		printf("number not found");
+    }
+    return 0;
+}
